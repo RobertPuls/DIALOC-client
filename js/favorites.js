f039966ae930ac021f85fa635ffd2e3835677d36
Robert Puls <rpu1s@sbcglobal.net>
2017-06-29 18:39:50 -0600

    fix favorites on favorites page

--- js/favorites.js
+++ js/favorites.js
@@ -1,4 +1,51 @@
 $(() => {
+  function antipode(coord) {
+    return [-1 * coord[0], coord[1] - 180]
+  }
+
+  function addFavoriteImage(homeCoords, hero = "col s5 amber lighten-5") {
+    let imgURL = [];
+    let maxZoom = 9;
+
+    homeCoords.push(antipode(homeCoords[0]));
+
+    console.log("home", homeCoords);
+
+    for (let i = 0; i < 2; i++) {
+      let e = {
+        lat: homeCoords[i][0],
+        lng: homeCoords[i][1]
+      };
+
+      let maxZoomService = new google.maps.MaxZoomService();
+      console.log(e);
+      maxZoomService.getMaxZoomAtLatLng(e, function(response) {
+        maxZoom = response.zoom;
+        imgURL.push(`https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=${homeCoords[i].toString()}&zoom=${maxZoom-1}&size=350x350&key=AIzaSyAiB8Q6zW5qm1u2d5LKrT98udr4wbQKEuk`);
+        if (i == 1) {
+          $("#favImage").append(`
+            <a href="#modal1" class="roundedBorder lessImage ${hero} card-panel valign-wrapper activator">
+                <div class="col s6">
+                  <img src="${imgURL[0]}" alt="" class="circle responsive-img favorite-img">
+                  <p class="red-text">${homeCoords[0]}</p>
+                </div>
+                <div class="col s6">
+                  <img src="${imgURL[1]}" alt="" class="circle responsive-img favorite-img">
+                  <p class="red-text">${homeCoords[1]}</p>
+                </div>
+              </a>`);
+        }
+      });
+    }
+    $('.modal').modal();
+
+  $(".col").on("click", function() {
+    console.log(this);
+    $("#modalBody").html("");
+    $("#modalBody").html(`Enter Info Here`);
+  });
+}
+
   $.ajaxSetup({
     crossDomain: true,
     headers: {
@@ -24,39 +71,13 @@ $(() => {
 
   })
   $.get('https://dialoc-server.herokuapp.com/location').then((res) => {
-      console.log(res)
-      return res
-    }).then((x) => addFavorites([
-      [
-        [39.7578, -105.0072],
-        [-44.7584, 192.2543]
-      ],
-      [
-        [50.7578, -105.0072],
-        [-50.7584, 192.1819]
-      ],
-      [
-        [70.7578, -115.0072],
-        [-30.7584, 200.1764]
-      ],
-      [
-        [70.7578, -115.0072],
-        [-30.7584, 200.1237]
-      ]
-    ])).then(() => initialize(
-      [
-        [39.7578, -105.0072],
-        [-44.7584, 192.2543]
-      ], [
-        [50.7578, -105.0072],
-        [-50.7584, 192.1819]
-      ], [
-        [70.7578, -115.0072],
-        [-30.7584, 200.1764]
-      ], [
-        [70.7578, -115.0072],
-        [-30.7584, 200.1237]
-      ]));
+    return res.reduce((acc, locObj) => {
+        acc.push([locObj.latitude, locObj.longitude]);
+        return acc;
+      },[])
+    }).then((acc) => acc.map(coords => {
+      addFavoriteImage([coords]);
+    }));
   // initialize()
   var wmStreetViewKey = 'AIzaSyCuPQR1KWE3uYIoml6bzBOTrA78iVIeaRI'
   var wmPlacesKey = 'AIzaSyDBNBysOcc4ZOhnnHVW_LSMSYBgn9p1YE4'
@@ -74,42 +95,13 @@ $(() => {
       var markerCustom = WE.marker(coord, '/images/bullet_orange.png', 8, 8).addTo(earth)
       var markerCustom2 = WE.marker(antip, '/images/bullet_pink.png', 8, 8).addTo(earth)
     }
-
-    // function antipode(coord) {
-    //   return [-1 * coord[0], coord[1] - 180]
-    // }
     // let coords = [39.7578, -105.0072]
     // let antip = antipode(coords)
     // var markerCustom = WE.marker(coords, '/images/bullet_orange.png', 8, 8).addTo(earth)
     // var markerCustom2 = WE.marker(antip, '/images/bullet_pink.png', 8, 8).addTo(earth)
 
     earth.setView([39.7578, -105.0072], .8);
-    // addFavorites([
-    //   [
-    //     [39.7578, -105.0072],
-    //     [-44.7584, 192.2543]
-    //   ],
-    //   [
-    //     [50.7578, -105.0072],
-    //     [-50.7584, 192.1819]
-    //   ],
-    //   [
-    //     [70.7578, -115.0072],
-    //     [-30.7584, 200.1764]
-    //   ],
-    //   [
-    //     [70.7578, -115.0072],
-    //     [-30.7584, 200.1237]
-    //   ]
-    // ])
-
-
-
-
-
 
   }
 
-
-
-})
+});
